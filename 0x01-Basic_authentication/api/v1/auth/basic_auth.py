38c22ac7c8009402c77bea1355fc590ee34dc984
Abdellatif <abdelt.2012@gmail.com>
2024-07-09 22:01:36 +0100

    Done.

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -13,4 +13,13 @@ from models.user import User
 class BasicAuth(Auth):
     """Basic authentication class.
     """
-    pass
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:  # noqa
+        """
+        Extracts the Base64 part of the Authorization header for Basic
+        Authentication.
+        """
+        if authorization_header is None or not isinstance(authorization_header, str):  # noqa
+            return None
+        if authorization_header.startswith("Basic "):
+            return authorization_header.split(" ", 1)[1]
+        return None
