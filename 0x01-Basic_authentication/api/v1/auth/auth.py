6e73d069c1b9157677ee065eb945f2dfa3f7bf84
Abdellatif <abdelt.2012@gmail.com>
2024-07-09 21:42:59 +0100

    Done.

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -2,6 +2,7 @@
 """
 Auth.py file.
 """
+import re
 from flask import request
 from typing import List, TypeVar
 
@@ -24,7 +25,18 @@ class Auth:
         """
         Placeholder method for checking if authentication is required.
         """
-        return False
+        if path is not None and excluded_paths is not None:
+            for exclusion_path in map(lambda x: x.strip(), excluded_paths):
+                pattern = ''
+                if exclusion_path[-1] == '*':
+                    pattern = '{}.*'.format(exclusion_path[0:-1])
+                elif exclusion_path[-1] == '/':
+                    pattern = '{}/*'.format(exclusion_path[0:-1])
+                else:
+                    pattern = '{}/*'.format(exclusion_path)
+                if re.match(pattern, path):
+                    return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """
