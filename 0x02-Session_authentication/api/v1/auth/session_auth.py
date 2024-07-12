faa5e9c3757304df8c6f4115a83e2e8fbcf239c3
Abdellatif <abdelt.2012@gmail.com>
2024-07-12 01:53:19 +0100

    Done.

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Session Authentication
+"""
+from .auth import Auth
+
+
+class SessionAuth(Auth):
+    """                                                                                                                     A class SessionAuth that inherits from Auth.
+    """
+    pass
