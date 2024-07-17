f11e6c253179273c8cd367cab49a9e808100a916
Abdellatif <abdelt.2012@gmail.com>
2024-07-17 03:30:45 +0100

    Done.

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+user.py file.
+"""
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class User(db.Model):
+    """
+    A SQLAlchemy model named User for a database table named users:
+    The model will have the following attributes:
+        - id, the integer primary key
+        - email, a non-nullable string
+        - hashed_password, a non-nullable string
+        - session_id, a nullable string
+        - reset_token, a nullable string
+    """
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(250), nullable=False, unique=True)
+    hashed_password = db.Column(db.String(250), nullable=False)
+    session_id = db.Column(db.String(250), nullable=True)
+    reset_token = db.Column(db.String(250), nullable=True)
+
+    def __repr__(self):
+        return f'<User {self.email}>'
