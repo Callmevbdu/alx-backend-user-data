8f9af9fe060e0f0fc7b0fc3ee6794dfc8b99b3af
Abdellatif <abdelt.2012@gmail.com>
2024-07-17 03:48:53 +0100

    Done.

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,10 +1,10 @@
+#!/usr/bin/env python3
 """DB module
 """
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from sqlalchemy.orm.exc import NoResultFound
 
 from user import Base, User
 
