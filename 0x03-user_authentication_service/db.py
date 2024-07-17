4481091a5d37006767dccace1e3d0ac2099bb578
Abdellatif <abdelt.2012@gmail.com>
2024-07-17 03:47:30 +0100

    Done.

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,49 @@
+"""DB module
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
+
+from user import Base, User
+
+
+class DB:
+    """DB class
+    """
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """
+        - Note that DB._session is a private property and hence should NEVER
+        be used from outside the DB class.
+        - Implement the add_user method, which has two required string
+        arguments: email and hashed_password, and returns a User object. The
+        method should save the user to the database. No validations are
+        required at this stage.
+        """
+        try:
+            user = User(email=email, hashed_password=hashed_password)
+            self._session.add(user)
+            self._session.commit()
+        except Exception:
+            self._session.rollback()
+            user = None
+        return user
